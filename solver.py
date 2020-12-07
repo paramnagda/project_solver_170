101a8561fc005b053e0679c358ebe909e6924771
Param Nagda <paramnagda@berkeley.edu>
2020-12-07 18:05:49 +0530

    implemented naive solution which solved many imediums and larges

--- solver.py
+++ solver.py
@@ -4,7 +4,7 @@ from utils import *
 import sys
 import os.path
 from os.path import basename, normpath
-import os.path
+import time
 import glob
 from itertools import permutations
 from mip import *
@@ -53,10 +53,8 @@ def solve(G, s, h = 0):
         print("checkpoint1")
         print('model has {} vars, {} constraints and {} nzs'.format(m.num_cols, m.num_rows, m.num_nz))
         m += xsum(sameroom[i][j]*stress_tri[i][j] for i in range(numnodes - 1) for j in range(numnodes-i-1)) <= s
-        #m += xsum(sameroom[i][j]*stress_tri[i][j] for i in range(numnodes - 1) for j in range(numnodes-i-1)) >= (stress_sum + s)/2
-        # THE NEXT LINE IS THE LINE TO ADD CONSTRAINTS IF WE PURSUE SCRAPING
-        m += xsum(sameroom[i][j]*happiness_tri[i][j] for i in range(numnodes - 1) for j in range(numnodes-i-1)) <= happiness_sum - happiness_sum * 0.005
-
+        m += xsum(sameroom[i][j]*happiness_tri[i][j] for i in range(numnodes - 1) for j in range(numnodes-i-1)) <= happiness_sum - happiness_sum * 0.001
+        # m += xsum(sameroom[i][j]*happiness_tri[i][j] for i in range(numnodes - 1) for j in range(numnodes-i-1)) <= <INSERT VALUE TO SCRAPE HERE>  
         
         for i in range(numnodes-2):
             for j in range(i+1,numnodes-1):
@@ -69,8 +67,8 @@ def solve(G, s, h = 0):
                     m += sameroom[i][j-i-1] + sameroom[i][k-i-1] <= sameroom[j][k-j-1]  + 1
                     m += sameroom[i][k-i-1] + sameroom[j][k-j-1] <= sameroom[i][j-i-1]  + 1
 
-        # for i in range(numnodes):
-        #     m += xsum(sameroom[i][j] for j in range(numnodes - i - 1)) + xsum(sameroom[j][i-j-1] for j in range(i+1,numnodes-1))
+        for i in range(numnodes):
+            m += xsum(sameroom[i][j] for j in range(numnodes - i - 1)) + xsum(sameroom[j][i-j-1] for j in range(i-1)) >= 1
         
         minstress = minimumstress(G)
         maxstress = maximumstress(G)
@@ -101,7 +99,7 @@ def solve(G, s, h = 0):
             currroom = 0
             flag = [False for _ in range(numnodes)]
             flag[0] = True
-            for i in range(numnodes-1):
+            for i in range(numnodes):
                 if not flag[i]:
                     flag[i] = True
                     currroom += 1
@@ -130,6 +128,39 @@ def solve(G, s, h = 0):
             print("failed budget: ", s)
             return 0, 0
     return 0, 0
+
+def solverNotOptimal(G, s):
+    numnodes = G.number_of_nodes()
+    valid = False
+    sperroom = s/2
+    pendingstudents = list(G.nodes())
+    curroom = 0
+    roomsmapping = {}
+    for student in pendingstudents:
+        roomsmapping[student] = curroom
+        curroom += 1
+        pendingstudents.remove(student)
+        print(student)
+        print(pendingstudents)
+        potentialpartners = [partner for partner in pendingstudents if G[student][partner]["stress"] <= sperroom]
+        if potentialpartners:
+            partner = max(potentialpartners, key = lambda x: G[student][x]["happiness"])
+            roomsmapping[partner] = roomsmapping[student]
+            pendingstudents.remove(partner)
+    rooms = len(set(roomsmapping.values()))
+    valid = is_valid_solution(roomsmapping, G, s, rooms)
+    print("ROOMS:", rooms)
+    print("budget: ", s)
+    print("Valid:", valid)
+    print(roomsmapping)
+    if valid:
+        print("VALIDDDDD")
+        return roomsmapping, rooms
+    else:
+        print("INVALID: Could not find appropriate matching")
+        return 0, 0
+        
+        
 
 def pairidentifier(i, j, n):
     return i*n + j
@@ -169,59 +200,72 @@ def maximumstress(G):
         
 #         for i in list(perm):
 
-def writeupdate(solved, unsolved):
+def writeupdate(solved, unsolved, skippedcozexists):
     with open("update.txt", "w") as fo:
-        fo.write("solved in this iteration:\n")
+        fo.write("**solved in this iteration:**\n")
         for key, value in solved.items():
             fo.write(str(key) + " " + str(value) + "\n")
-        fo.write("skipped in this iteration:\n")
+        fo.write("**skipped in this iteration:**\n")
         for key, value in unsolved.items():
             fo.write(str(key) + " " + str(value) + "\n")
+        fo.write("**skipped coz exists:**\n")        
+        for skipped in skippedcozexists:
+            fo.write(skipped + "\n")
+        fo.write("**total solved in this iteration**" + str(len(solved.keys())) + "\n")
+        fo.write("**total unsolved in this iteration**" + str(len(unsolved.keys())) + "\n")   
+        fo.write("**total skipped coz exists in this iteration**" + str(len(skippedcozexists)) + "\n")
+   
         fo.close()
 
 
 if __name__ == '__main__':
+    start = time.time()
     if (len(sys.argv) == 2):
         path = sys.argv[1]
         G, s = read_input_file(path)
-        output_path = 'outputs/' + basename(normpath(path))[:-3] + '.out'
-        D, k = solve(G, s)
+        output_path = 'naiveoutputs/' + basename(normpath(path))[:-3] + '.out'
+        D, k = solverNotOptimal(G, s)
         if D or k:
             assert is_valid_solution(D, G, s, k)
             happiness = calculate_happiness(D, G)
+            print("happiness: ", calculate_happiness(D, G))
             write_output_file(D, output_path)
         else:
             print("NO SOLUTION FOUND FOR", path)
     else:
         unsolveddict = {}
         happinessdict = {}
+        skippedcozexists = []
         inputs = glob.glob('inputs/*')
         for input_path in inputs:
-            if "small" in input_path:
-                output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
+            if "large" in input_path:
+                output_path = 'naiveoutputs/' + basename(normpath(input_path))[:-3] + '.out'
                 if os.path.isfile(output_path):
                     print("skipping", output_path, "because it already exists")
+                    skippedcozexists += [output_path]
+                    writeupdate(happinessdict, unsolveddict, skippedcozexists)
                     continue
-                    writeupdate(happinessdict, unsolveddict)
                 else:
                     print("now attempting", input_path)
                     G, s = read_input_file(input_path)
                     try:
-                        D, k = solve(G, s)
+                        D, k = solverNotOptimal(G, s)
                         if D or k:
                             assert is_valid_solution(D, G, s, k)
                             happiness = calculate_happiness(D, G)
                             write_output_file(D, output_path)
                             happinessdict[input_path] = happiness
                         else:
-                            unsolveddict[input_path] = "timed out"
+                            unsolveddict[input_path] = "did not give valid solution"
                         print("solved list", happinessdict)
                         print("unsolved list", unsolveddict)
-                        writeupdate(happinessdict, unsolveddict)
+                        writeupdate(happinessdict, unsolveddict, skippedcozexists)
                     except Exception:
                         unsolveddict[input_path] = "gave error"
-                        writeupdate(happinessdict, unsolveddict)
+                        writeupdate(happinessdict, unsolveddict, skippedcozexists)
                         continue
+    end = time.time()
+    print("total time taken:", end - start)
 
 
         
